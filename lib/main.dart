95b60b64699bca4e4fd29610bd9170f00bc84120
andrekarkoski <andreskarkoski@gmail.com>
2021-04-09 14:52:31 -0300

    Loja Virtual

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/admin_users_manager.dart';
+import 'package:loja_virtual/models/cart_manager.dart';
+import 'package:loja_virtual/models/home_manager.dart';
+import 'package:loja_virtual/models/product.dart';
+import 'package:loja_virtual/models/product_manager.dart';
+import 'package:loja_virtual/models/user_manager.dart';
+import 'package:loja_virtual/screens/base/base_screen.dart';
+import 'package:loja_virtual/screens/cart/cart_screen.dart';
+import 'package:loja_virtual/screens/edit_product/edit_product_screen.dart';
+import 'package:loja_virtual/screens/login/login_screen.dart';
+import 'package:loja_virtual/screens/product/product_screen.dart';
+import 'package:loja_virtual/screens/signup/signup_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserManager(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ProductManager(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider(
+          create: (_) => HomeManager(),
+          lazy: false,
+        ),
+        ChangeNotifierProxyProvider<UserManager, CartManager>(
+          create: (_) => CartManager(),
+          lazy: false,
+          update: (_, userManager, cartManager) =>
+            cartManager..updateUser(userManager),
+        ),
+        ChangeNotifierProxyProvider<UserManager, AdminUsersManager>(
+          create: (_) => AdminUsersManager(),
+          lazy: false,
+          update: (_, userManager, adminUsersManager) =>
+          adminUsersManager..updateUser(userManager),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Varejão das Fabricas',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 4, 125, 141),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+          appBarTheme: const AppBarTheme(
+              elevation: 0
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/base',
+        onGenerateRoute: (settings){
+          switch(settings.name){
+            case '/login':
+              return MaterialPageRoute(
+                  builder: (_) => LoginScreen()
+              );
+            case '/signup':
+              return MaterialPageRoute(
+                  builder: (_) => SignUpScreen()
+              );
+            case '/product':
+              return MaterialPageRoute(
+                  builder: (_) => ProductScreen(
+                    settings.arguments as Product
+                  )
+              );
+            case '/cart':
+              return MaterialPageRoute(
+                  builder: (_) => CartScreen()
+              );
+            case '/edit_product':
+              return MaterialPageRoute(
+                  builder: (_) => EditProductScreen(
+                    settings.arguments as Product
+                  )
+              );
+            case '/base':
+            default:
+              return MaterialPageRoute(
+                  builder: (_) => BaseScreen()
+              );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:loja_virtual/models/user_manager.dart';
+import 'package:loja_virtual/screens/admin_users_screen/admin_users_screen.dart';
+import 'package:loja_virtual/screens/home/home_screen.dart';
+import 'package:loja_virtual/screens/login/login_screen.dart';
+import 'package:loja_virtual/screens/products/products_screen.dart';
+import 'package:provider/provider.dart';
+
+class BaseScreen extends StatelessWidget {
+
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: Consumer<UserManager>(
+        builder: (_, userManager, __){
+          return PageView(
+            controller: pageController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              ProductsScreen(),
+              Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                  title: const Text('Home3'),
+                ),
+              ),
+              Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                  title: const Text('Home4'),
+                ),
+              ),
+              if(userManager.adminEnabled)
+                ...[
+                  AdminUsersScreen(),
+                  Scaffold(
+                    drawer: CustomDrawer(),
+                    appBar: AppBar(
+                      title: const Text('Pedidos'),
+                    ),
+                  ),
+                ]
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
